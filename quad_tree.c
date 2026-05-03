7ec3ef4da4ee2655edb61c62e7f873c9c71bd414
Dobby <thomas.dobbelaere@uclouvain.be>
2026-05-03 22:02:36 +0200

    create quad_tree struct

--- quad_tree.c
+++ quad_tree.c
@@ -7,11 +7,13 @@
 
 typedef double f64;
 typedef uint32_t u32;
+typedef uint64_t u64;
 typedef u32 b32;
 typedef struct cell_struct cell;
 
 typedef struct {
   u32 n;
+  u64 id[MAXP];
   f64 x[MAXP][DIM];
 } leaf_data;
 
@@ -22,21 +24,27 @@ struct cell_struct {
   cell *sub;
 };
 
+typedef struct {
+  u64 size;
+  cell *root;
+} quad_tree;
+
 void cell_init(cell *tree, const f64 *xmin, const f64 *xmax);
+b32 cell_add(cell *c, u64 id, const f64 *x);
 void cell_free(cell *c);
 void cell_split(cell *tree);
 b32 cell_contains(cell *c, const f64 *x);
 
-cell *tree_create(const f64 *xmin, const f64 *xmax);
-void tree_free(cell *c);
-b32 tree_add(cell *c, const f64 *x);
+quad_tree tree_create(const f64 *xmin, const f64 *xmax);
+void tree_free(quad_tree *tree);
+b32 tree_add(quad_tree *tree, const f64 *x);
 
 int main(void) { return 0; }
 
-cell *tree_create(const f64 *xmin, const f64 *xmax) {
-  cell *tree = malloc(sizeof(cell));
-  cell_init(tree, xmin, xmax);
-  return tree;
+quad_tree tree_create(const f64 *xmin, const f64 *xmax) {
+  cell *root = malloc(sizeof(cell));
+  cell_init(root, xmin, xmax);
+  return (quad_tree){.size = 0, .root = root};
 }
 
 void cell_init(cell *tree, const f64 *xmin, const f64 *xmax) {
@@ -57,12 +65,20 @@ b32 cell_contains(cell *c, const f64 *x) {
   return true;
 }
 
-b32 tree_add(cell *c, const f64 *x) {
+b32 tree_add(quad_tree *tree, const f64 *x) {
+  b32 added = cell_add(tree->root, tree->size, x);
+  if (added)
+    tree->size++;
+  return added;
+}
+
+b32 cell_add(cell *c, const u64 id, const f64 *x) {
   if (!cell_contains(c, x))
     return false;
   if (c->leaf) {
     u32 ip = c->leaf->n;
     if (ip < MAXP) {
+      c->leaf->id[ip] = id;
       f64 *xp = c->leaf->x[ip];
       for (u32 i = 0; i < DIM; ++i) {
         xp[i] = x[i];
@@ -75,7 +91,7 @@ b32 tree_add(cell *c, const f64 *x) {
   if (c->sub) {
     u32 nsub = 1 << DIM;
     for (u32 i = 0; i < nsub; ++i) {
-      if (tree_add(&c->sub[i], x))
+      if (cell_add(&c->sub[i], id, x))
         return true;
     }
   }
@@ -94,13 +110,20 @@ void cell_split(cell *c) {
     f64 b[2] = {(i % 2) ? c->xmax[0] : xc[0], (i / 2) ? c->xmax[1] : xc[1]};
     cell_init(&c->sub[i], a, b);
   }
+  for (u32 j = 0; j < c->leaf->n; ++j) {
+    for (u32 i = 0; i < nsub; ++i) {
+      b32 added = cell_add(&c->sub[j], c->leaf->id[j], c->leaf->x[j]);
+      if (added)
+        break;
+    }
+  }
   free(c->leaf);
   c->leaf = NULL;
 }
 
-void tree_free(cell *c) {
-  cell_free(c);
-  free(c);
+void tree_free(quad_tree *tree) {
+  cell_free(tree->root);
+  free(tree->root);
 }
 
 void cell_free(cell *c) {
